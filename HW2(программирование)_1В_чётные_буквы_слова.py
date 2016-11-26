8480dfa0fe421367feadc8a3829d53238ae8e2aa
CarolineFs <dr_gromova1998@mail.ru>
2016-11-26 12:09:30 +0300

    Update HW2(программирование)_1В_чётные_буквы_слова.py

--- HW2(программирование)_1В_чётные_буквы_слова.py
+++ HW2(программирование)_1В_чётные_буквы_слова.py
@@ -2,10 +2,11 @@ w = input('Введите слово (в кириллице): ')
 wl=w.lower()
 a='а'
 k='к'
-for a in wl:
-    wlra=wl.replace('а',' ')
-for k in wlra:
-    wlrak=wlra.replace('к', ' ')
-r=wlrak[1: : 2]
-rs=r.replace(' ','')
+if a in wl:
+    wl=wl.replace('а',' ')
+if  k in wl:
+    wl=wl.replace('к', ' ')
+r=wl[1: : 2]
+r=r.replace(' ','')
 print ('Чётные буквы введенного слова, кроме "а" и "к":',r)
+
