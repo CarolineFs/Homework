3030e7f6bab13dc09e4cdb13278d3bdaa69efecb
CarolineFs <dr_gromova1998@mail.ru>
2017-01-20 20:45:35 +0300

    Create HW8_B1.py

---
+++ HomeworkPython/HW8_B1.py
@@ -0,0 +1,24 @@
+import csv
+import random
+clues = {}
+f = open('clues.csv', 'r', encoding='utf-8') 
+text = csv.reader(f, delimiter=';')
+for word in text:
+    clues[word[0]] = word[1]
+values = list(clues.values())
+keys = list(clues.keys())
+n = random.choice([0, 1,2,3,4,5,6,7,8,9,10])
+l = len (values[n])
+k = 0
+i = 1
+while i <= l:
+    response = input (keys[n]+' '+'...'+' ')
+    k += 1
+    i += 1
+    if response == values [n]:
+        print('Вы победили!')
+        break
+    else:
+        print ('Нет!')
+if response != values[n]:
+    print('Вы проиграли!')
