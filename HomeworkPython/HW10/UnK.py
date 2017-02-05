522dad167a3b137bbe3b3eb88af62e964ab78259
CarolineFs <dr_gromova1998@mail.ru>
2017-02-05 18:55:04 +0300

    Update UnK.py

--- HomeworkPython/HW10/UnK.py
+++ HomeworkPython/HW10/UnK.py
@@ -1,7 +1,7 @@
 import re
 with open ('Великобритания — Википедия.html', 'r', encoding = 'utf - 8') as f:
     text = f.read()
-card = '<tr>\n<td>(?:.)+title="Столица(?:.)+\n(?:.)+title="(.*)"'
+card = '<tr>\n<td>(?:.)+title="Столица(?:.)+\n(?:.)+title="([А-Я][а-я]+)?"'
 lines = re.search(card, text)
 l = lines.group(1)
 with open ('results.txt', 'w', encoding = 'utf - 8') as res:
